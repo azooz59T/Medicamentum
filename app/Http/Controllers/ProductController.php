d344c8a565b9f4c609489cae7340f6b01780c869
azooz59T <mohamedoo1997@hotmail.com>
2025-09-19 19:56:53 +0300

    Return products through view in Controller
    + View is yet to be created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::paginate(8); // Paginate with 8 items per page
+
+        return view('products.index', compact('products'));
+    }
 }
