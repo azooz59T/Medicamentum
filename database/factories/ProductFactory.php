ddf9414505c9fa9085c4b108e6facbc36df5bd0e
azooz59T <mohamedoo1997@hotmail.com>
2025-09-21 13:13:10 +0300

    Add factory for description and category

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -26,6 +26,11 @@ class ProductFactory extends Factory
         return [
             'name' => $this->faker->unique()->word() . ' ' . $this->faker->unique()->word(),
             'price' => $this->faker->randomFloat(2, 0, 999), // Generates a random price with 2 decimal places, between 1 and 999
+            'category' => $this->faker->randomElement([
+            'Dressings', 'Strapping', 'Tablets', 'Insoles', 
+            'Gloves, masks, aprons', 'Steriliser trays and pouches', 'Wheelchair'
+            ]),
+            'description' => $this->faker->sentence(10)
         ];
     }
 }
