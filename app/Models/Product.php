923a304ed4043fa5a08dc8e699f82cc89a4ab919
azooz59T <mohamedoo1997@hotmail.com>
2025-09-21 12:51:15 +0300

    UpdateProduct model to allow for mass assignment

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Product extends Model
 {
     use HasFactory;
+
+    // make the following attributes mass assigned when the product is being created on by admin
+    protected $fillable = [
+        'name',
+        'category',
+        'description',
+        'image',
+        'price'
+    ];
+
+    // converts an attribute's value to a more usable data type from the database
+    protected $casts = [
+        'price' => 'decimal:2'
+    ];
 }
