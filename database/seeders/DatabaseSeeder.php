0460a3bb24e18dc577695538fee78582ef5a75b4
azooz59T <mohamedoo1997@hotmail.com>
2025-09-22 12:38:38 +0300

    Add Admin Seed to the database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,9 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
+        $this->call([
+            AdminUserSeeder::class,
+        ]);
         $this->call(ProductSeeder::class);
     }
 }
